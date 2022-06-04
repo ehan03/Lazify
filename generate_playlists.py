e1a04b8e9f557b14fbf52a40d9da55e6701ed6bb
ehan03 <eugeneh442@gmail.com>
2022-06-03 23:38:28 -0700

    Generate playlists checkpoint

--- generate_playlists.py
+++ generate_playlists.py
@@ -1,7 +1,6 @@
 # from sklearn.cluster import KMeans
 import spotipy as sp
 import pandas as pd
-import numpy as np
 
 def cluster(user_id, selected_playlists):
     pass
@@ -12,8 +11,11 @@ def semantic(user_id, selected_playlists):
 def artists(user_id, selected_playlists):
     pass
 
+# Merge two or more playlists into one
+# Adds only unique tracks so playlists with overlapping tracks aren't problematic
 def merge(spotify, user_id, selected_playlists):
     # In case user only selected one playlist, don't do anything and return playlist id
+    # To-do: Consider checking beforehand at option selection page
     if len(selected_playlists) == 1:
         return selected_playlists
 
@@ -33,20 +35,23 @@ def merge(spotify, user_id, selected_playlists):
 
     return [make_playlist(spotify, user_id, new_name, uris)]
 
+# Remove duplicate tracks from playlists
 def remove_duplicates(spotify, user_id, selected_playlists):
     for playlist in selected_playlists:
         uris = get_track_uris(spotify, user_id, playlist)
         unique_uris = list(set(uris))
 
+        # Skip playlists that don't contain duplicates
         if len(unique_uris) == len(uris):
             continue
-
+        
+        # Modify playlist in place
         spotify.user_playlist_replace_tracks(user_id, playlist, unique_uris)
 
     return selected_playlists
 
 # Retrieve only track uris for playlists
-# To be used for merge and remove duplicates since audio features are not needed
+# To be used for merge and remove duplicates since audio features aren't needed
 def get_track_uris(spotify, user_id, playlist):
     results = spotify.user_playlist_tracks(user_id, playlist)
     tracks = results['items']
@@ -61,8 +66,13 @@ def get_track_uris(spotify, user_id, playlist):
 
     return uris
 
+# Retrieve everything about each track in a playlist
+# Includes name, artist, uri, and audio features
+def get_track_features(spotify, user_id, playlist):
+    pass
+
+# Create playlist given a name and a list of track uris
 def make_playlist(spotify, user_id, name, uris):
-    # Create playlist
     playlist_id = spotify.user_playlist_create(user_id, name, public=False)['id']
 
     offset = 0
@@ -72,6 +82,7 @@ def make_playlist(spotify, user_id, name, uris):
 
     return playlist_id
 
+# Main driver
 def generate(option, spotify, user_id, selected_playlists):
     options = {
         "cluster": cluster,
