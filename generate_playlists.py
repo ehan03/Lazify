3f4622879e7f34672847f1563f22ab1ef2e50000
ehan03 <eugeneh442@gmail.com>
2022-06-03 23:17:34 -0700

    Updated merge and finished remove duplicates

--- generate_playlists.py
+++ generate_playlists.py
@@ -13,55 +13,61 @@ def artists(user_id, selected_playlists):
     pass
 
 def merge(spotify, user_id, selected_playlists):
+    # In case user only selected one playlist, don't do anything and return playlist id
+    if len(selected_playlists) == 1:
+        return selected_playlists
+
     # Get unique track uris
-    tracks = list(set(get_track_uris(spotify, user_id, selected_playlists)))
+    uris = []
+    for playlist in selected_playlists:
+        uris.extend(get_track_uris(spotify, user_id, playlist))
+    
+    uris = list(set(uris))
 
     # Get names of selected playlists
     names = []
     for playlist in selected_playlists:
         names.append(spotify.user_playlist(user_id, playlist)['name'])
 
-    # Create playlist
     new_name = "Merged " + " + ".join(names)
 
-    # Work around so that returned object is iterable
-    res = []
-    res.append(make_playlist(spotify, user_id, new_name, tracks))
-    return res
+    return [make_playlist(spotify, user_id, new_name, uris)]
 
+def remove_duplicates(spotify, user_id, selected_playlists):
+    for playlist in selected_playlists:
+        uris = get_track_uris(spotify, user_id, playlist)
+        unique_uris = list(set(uris))
 
-def remove_duplicates(user_id, selected_playlists):
-    pass
+        if len(unique_uris) == len(uris):
+            continue
+
+        spotify.user_playlist_replace_tracks(user_id, playlist, unique_uris)
+
+    return selected_playlists
 
 # Retrieve only track uris for playlists
 # To be used for merge and remove duplicates since audio features are not needed
-def get_track_uris(spotify, user_id, playlists):
-    # Get tracks from selected playlists
-    tracks = []
-    for playlist in playlists:
-        results = spotify.user_playlist_tracks(user_id, playlist)
-        tracks.extend(results['items'])
-
-        while results['next']:
-            results = spotify.next(results)
-            tracks.extend(results['items'])
-    
-    # Get track uris
+def get_track_uris(spotify, user_id, playlist):
+    results = spotify.user_playlist_tracks(user_id, playlist)
+    tracks = results['items']
     uris = []
+
+    while results['next']:
+        results = spotify.next(results)
+        tracks.extend(results['items'])
+    
     for track in tracks:
         uris.append(track['track']['uri'])
-    
+
     return uris
 
-def make_playlist(spotify, user_id, name, tracks):
+def make_playlist(spotify, user_id, name, uris):
     # Create playlist
     playlist_id = spotify.user_playlist_create(user_id, name, public=False)['id']
 
-    # Add tracks to playlist
-    # 100 song limit per request
     offset = 0
-    while offset < len(tracks):
-        spotify.user_playlist_add_tracks(user_id, playlist_id, tracks[offset:offset+100])
+    while offset < len(uris):
+        spotify.user_playlist_add_tracks(user_id, playlist_id, uris[offset:offset+100])
         offset += 100
 
     return playlist_id
@@ -76,9 +82,3 @@ def generate(option, spotify, user_id, selected_playlists):
     }
 
     return options[option](spotify, user_id, selected_playlists)
-
-
-
-### To create a playlist from list of tracks
-# user_playlist_create
-# user_playlist_add_tracks
